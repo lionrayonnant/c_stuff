36e0477b2aeae54ddd039ade3ffcce72b0887d8a
My name <myemail@example.com>
2025-10-24 16:38:20 +0200

    Add new thread programs

---
+++ 2-processus_primitives_threads_linux/src/threads_exo1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+/* Déclaration des fonctions */
+
+void *addition(void* args){
+   
+    int* arguments = (int*)args;
+
+    int a = arguments[0];
+    int b = arguments[1];
+    int x;
+	
+    void *ret1;
+
+    x=a+b;
+
+    printf("Addition de %d et de %d : %d\n", a, b, x);
+
+    pthread_exit(ret1);
+}
+
+void *multiplication(void* args) {
+   
+    int* arguments = (int*)args;
+
+    int a = arguments[0];
+    int b = arguments[1];
+    int x;
+
+    void *ret1;
+
+    x=a*b;
+
+    printf("Multiplication de %d et de %d : %d\n", a, b, x);
+
+    pthread_exit(ret1);
+}
+
+
+int main (){
+
+int args[2] = {6,7};
+void *ret2;
+void *ret1;
+
+/* Création des threads */
+
+pthread_t t_addition;
+pthread_create(&t_addition, NULL, addition, args);
+
+pthread_t t_multiplication;
+pthread_create(&t_multiplication, NULL, multiplication, args);
+
+pthread_join(t_addition, &ret1);
+pthread_join(t_multiplication, &ret2);
+
+printf("Le thread t_addition a retourné %d\n", ret1);
+printf("Le thread t_multiplication a retourné %d\n", ret2);
+return 0;
+}
